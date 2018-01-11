4829ca80c88833548b19ebdad32f9f1b0161c46d
gm300 <gmicro300@gmail.com>
2018-01-12 07:40:58 +0900

    update backup comment and file size displaying

--- up2val.py
+++ up2val.py
@@ -13,6 +13,9 @@ def cmd(a):
     if cmdsw == 1:
         print(a)
     else:
+        fw = open('.up2val.cmd.log','w')
+        fw.write(a)
+        fw.close()
         subprocess.getoutput(a)
 
 # you need to set your own Account and Vault Name in local.py file
@@ -26,9 +29,10 @@ from cdbackup import *
 fnl=sys.argv[1:-1]
 fnl.append(sys.argv[-1])
 for fn in fnl:
+    print("# "+fn)
     fs=os.path.getsize(fn)
     cs=fs if fs < csize else (int(fs/(int((fs-1)/csize)+1)/512)+1)*512
-
+    
     ft=open(fn,"rb")
     mt=TreeHash();mt.update(ft.read(1024*1024))
     ft.close()
@@ -40,8 +44,8 @@ for fn in fnl:
 
     ft=open(fn,"rb")
     cn=0
-    for x in range(0,fs,csize):
-        fs=ft.read(csize)
+    for x in range(0,fs,cs):
+        fs=ft.read(cs)
         fwn="tmp%2.2d" % cn
         fw=open(fwn,"wb") ;fw.write(fs) ;fw.close()
 
@@ -51,6 +55,7 @@ for fn in fnl:
         #mt=TreeHash();mt.update(fs); print("WSUM="+mt.hexdigest())
         cmd(("aws glacier upload-archive --account-id %s --vault-name %s --body %s " % (MyAccount,MyVault,fwn))+
             ("--archive-description %s/%d --checksum=%s" % (arcdsc,cn,csum)))
+        print(("#   part %d,%d byte" % (cn,len(fs))),flush=True)
         cn = cn+1
     ft.close()
 
