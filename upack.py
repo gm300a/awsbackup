30736d49d2dba291569cd63a40df5a8b37bdcd6b
gm300 <gmicro300@gmail.com>
2018-02-11 08:05:36 +0900

    clean up the code

--- upack.py
+++ upack.py
@@ -1,43 +1,44 @@
-import os.path,json,random,sys
-import hashlib
-from Crypto.Cipher import AES
-from random import randrange
-from datetime import datetime
+import os.path,json,sys
 from treehash import TreeHash
 
-cs=1024*1024*256
+from keygen import *
 
-fn=sys.argv[1]
-print(fn)
-fkey=fn[0:16]
+opt=dict()
+(cs,csh,opt['Verbose'])=(1024*1024*256,1024*16,('--verbose' in sys.argv))
 
-fh=open(fn,'rb')
-fs=os.path.getsize(fn)
+fnl=sys.argv[1:-1]
+fnl.append(sys.argv[-1])
 
-csh=1024*16
-fb=fh.read(csh)
-fe=AES.new (fkey, AES.MODE_CBC, 'This is an IV456').decrypt(fb)
-js=json.loads((fe.decode('utf-8')[10:10+int(fe.decode('utf-8')[0:10])]))
-print(js)
-mt=TreeHash()
-aes=AES.new (fkey, AES.MODE_CBC, 'This is an IV456')
+for x in fnl:
+    if x == '--verbose': continue
+    #fn=sys.argv[1]
+    fn='20180211.071103.meta'
+    print('# processing.. ',fn)
+    
+    (fh,fs)=(open(fn,'rb'),os.path.getsize(fn))
 
-fw=open(fn+'a','wb')
-#exit(1)
-cn=0
-cs0=js['Size']
-for x in range(0,js['Size'],cs):
-    fb=aes.decrypt(fh.read(cs))
-    if( cs0 < cs ):
-        mt.update(fb[0:cs0])
-        fw.write(fb[0:cs0])
-    else:
-        mt.update(fb)
-        fw.write(fb)
-    cn=cn+1
-    cs0=cs0-cs
-#meta['LongCRC'] = mt.hexdigest()
-#print("longcrs",mt.hexdigest())
-if mt.hexdigest() != js['LongCRC']:
-    print('long crs doesn\'t match')
-#print("pre cal","5b5087b45a90bf7e5e7d4e57795aaac5893d57d2b274f5b450c3de082e5a7236")
+    fb=fh.read(csh)
+    fe=DecMethod(fn).decrypt(fb).decode('utf-8')
+    js=json.loads((fe[10:10+int(fe[0:10])]))
+
+    if opt['Verbose']: print(js)
+
+    (mt,dec)=(TreeHash(),DecMethod(fn))
+    if opt['Verbose']:
+        print('## because verbose option is specified, decoding results is output to %sa' % fn)
+        fw=open(fn+'a','wb')
+    
+    for x in range(js['Size'],0,-cs):
+        fb=dec.decrypt(fh.read(cs))
+        if( cs <= x ): mt.update(fb)
+        else:     mt.update(fb[0:x])
+        if not opt['Verbose']: continue
+        if( cs <= x ): fw.write(fb)
+        else:    fw.write(fb[0:x])
+
+    if opt['Verbose']: fw.close()
+
+    if mt.hexdigest() == js['LongCRC']: continue
+    print('long crs doesn\'t match: File Name is ',fn)
+    exit(1)
+    
