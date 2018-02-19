aa5042ba9e11e6aadde8ff1066fac8e876f7ed7c
gm300 <gmicro300@gmail.com>
2018-02-20 06:57:57 +0900

    message update for input file name analysis

--- upack.py
+++ upack.py
@@ -1,4 +1,4 @@
-import os.path,json,sys
+import os.path,json,sys,re
 from treehash import TreeHash
 
 from keygen import *
@@ -14,11 +14,19 @@ for fn in fnl:
     #fn=sys.argv[1]
     #fn='20180211.071103.meta'
     print('# processing.. ',fn)
-    
+    m=re.search('^(.*/|)([0-9][0-9][0-9][0-9])([0-9][0-9])([0-9][0-9])\.([0-9][0-9])([0-9][0-9])([0-9][0-9])\.meta',fn)
+    if m == None:
+        print('file name error. should be 21341234.0123456.meta. no leading \'dot\', \'/ or full path')
+        exit(1)
+    elif int(m[2])<2017 or 2020<int(m[2]) or 12 < int(m[3]) or 31 < int(m[4]) or 24 < int(m[5]):
+        print('file name error. should like time stamp')
+        exit(1)
     (fh,fs)=(open(fn,'rb'),os.path.getsize(fn))
 
     fb=fh.read(csh)
     fe=DecMethod(fn).decrypt(fb).decode('utf-8')
+#    fe=DecMethod(fn).decrypt(fb)
+#    print(int(fe[0:10]))
     js=json.loads((fe[10:10+int(fe[0:10])]))
 
     if opt['Verbose']: print(js)
@@ -39,6 +47,6 @@ for fn in fnl:
     if opt['Verbose']: fw.close()
 
     if mt.hexdigest() == js['LongCRC']: continue
-    print('long crs doesn\'t match: File Name is ',fn)
+    print('error,long crs doesn\'t match: Orignal file Name in header is {}'.format(js['FileName']))
     exit(1)
     
